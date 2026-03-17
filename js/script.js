01ab460c0b8325918ebf7059736c3dd367ae325d
Tugu <200585649@student.georgianc.on.ca>
2026-03-16 23:24:26 -0400

    added energyBowl class(Tuguldur Natsagdorj)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
 // assigment 2 
 // Student 1 idris zahir - form handling
+// Student 2 Tugudur Natsagdorj 200585649
+class EnergyBowl {
+    constructor(base, fruits, toppings, protein, sweetener, size, instructions) {
+        this.base = base;
+        this.fruits = fruits;
+        this.toppings = toppings;
+        this.protein = protein;
+        this.sweetener = sweetener;
+        this.size = size;
+        this.instructions = instructions;
+    }
+
+    // method in order to describe the bowl
+    getDescription() {
+        return `
+        <strong>Base:</strong> ${this.base}<br>
+        <strong>Fruits:</strong> ${this.fruits.join(", ")}<br>
+        <strong>Toppings:</strong> ${this.toppings.join(", ")}<br>
+        <strong>Protein:</strong> ${this.protein}<br>
+        <strong>Sweetener:</strong> ${this.sweetener}<br>
+        <strong>Size:</strong> ${this.size}<br>
+        <strong>Instructions:</strong> ${this.instructions || "None"}
+        `;
+    }
+
+    // calculate prisze
+    calculatePrice() {
+        let price = 5; // base
+
+        // size pricing
+        if (this.size === "Medium") price += 2;
+        if (this.size === "Large") price += 4;
+
+        // add-ons
+        price += this.fruits.length * 1;
+        price += this.toppings.length * 0.5;
+
+        if (this.protein !== "None") price += 2;
+        if (this.sweetener !== "None") price += 1;
+
+        return price.toFixed(2);
+    }
+}
 
 
 // first we can listen if the user actually submits the form
@@ -53,3 +96,5 @@ document.getElementById("bowlForm").addEventListener("submit", function(event) {
 
 
 });
+
+
